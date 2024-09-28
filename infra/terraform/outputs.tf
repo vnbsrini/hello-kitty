c4b9c59b7912c209f2028d2c3d50819bc49356b4
stackgen-automation <automation@stackgen.com>
2024-09-28 10:01:32 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,42 +1,12 @@
 
-output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_srinivasan_balaji_28_arn" {
-  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.arn
+output "aws_iam_role_hello_kitty_role_srinivasan_balaji_28_name" {
+  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.name
   sensitive = false
 }
 
 
-output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_srinivasan_balaji_28_name" {
-  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
-  sensitive = false
-}
-
-
-output "aws_s3_srinivasan_balaji_28_kms_arn" {
-  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.kms_arn
-  sensitive = false
-}
-
-
-output "aws_s3_srinivasan_balaji_28_arn" {
-  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.arn
-  sensitive = false
-}
-
-
-output "aws_s3_srinivasan_balaji_28_bucket_name" {
-  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_name
-  sensitive = false
-}
-
-
-output "aws_s3_srinivasan_balaji_28_bucket_website_endpoint" {
-  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_website_endpoint
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_srinivasan_balaji_28_invoke_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
+output "aws_iam_role_hello_kitty_role_srinivasan_balaji_28_arn" {
+  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.arn
   sensitive = false
 }
 
@@ -59,13 +29,43 @@ output "aws_lambda_hello_kitty_function_srinivasan_balaji_28_function_url" {
 }
 
 
-output "aws_iam_role_hello_kitty_role_srinivasan_balaji_28_arn" {
-  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.arn
+output "aws_lambda_hello_kitty_function_srinivasan_balaji_28_invoke_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
   sensitive = false
 }
 
 
-output "aws_iam_role_hello_kitty_role_srinivasan_balaji_28_name" {
-  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.name
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_srinivasan_balaji_28_arn" {
+  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_srinivasan_balaji_28_name" {
+  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
+  sensitive = false
+}
+
+
+output "aws_s3_srinivasan_balaji_28_arn" {
+  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.arn
+  sensitive = false
+}
+
+
+output "aws_s3_srinivasan_balaji_28_bucket_name" {
+  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_name
+  sensitive = false
+}
+
+
+output "aws_s3_srinivasan_balaji_28_bucket_website_endpoint" {
+  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_srinivasan_balaji_28_kms_arn" {
+  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.kms_arn
   sensitive = false
 }
